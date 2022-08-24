29c3d3690e5bfb1e1e74d971efea01579c7d54eb
ByBaskentlim <104593816+ByBaskentlim@users.noreply.github.com>
2022-08-24 19:11:56 +0300

    Update index.php

--- index.php
+++ index.php
@@ -2,9 +2,9 @@
 require('youtube-dl.class.php');
 try
 {
-	$mytube = new yt_downloader("SmHRCvP6jq0");
+	$mytube = new yt_downloader("bWbssLithxY");
 
-	$mytube->set_audio_format("wav");        # Change default audio output filetype.
+	$mytube->set_audio_format("mp3");        # Change default audio output filetype.
 	$mytube->set_ffmpegLogs_active(FALSE);   # Disable Ffmpeg process logging.
 
 	$mytube->download_audio();
